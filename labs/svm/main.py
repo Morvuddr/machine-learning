bcc412efecdbce0b7e384cf478b1fee78be88b41
Igor Bopp <i.bopp@makeapp.ru>
2020-03-18 00:28:27 +0300

    Fix lab3

--- labs/svm/main.py
+++ labs/svm/main.py
@@ -6,15 +6,15 @@ import numpy as np
 import pandas as pd
 
 
-def colors (labels):
-    return ['green' if label == 'P' else 'red' for label in labels]
+def colors(labels):
+    return ['blue' if label == 'P' else 'red' for label in labels]
 
 
 def main():
     # regularization_strength = [0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]
     # kernel = ['linear', 'poly', 'rbf', 'sigmoid']
 
-    data = pd.read_csv("chips.csv")
+    data = pd.read_csv("geyser.csv")
     # data['class'] = pd.factorize(data['class'])[0]
     y = data['class'].values
     X = data.drop(['class'], axis=1).values
@@ -23,24 +23,17 @@ def main():
     #     for kern in kernel:
     #         svc = SVC(C=strength, kernel=kern)
     #         scores = cross_val_score(svc, X, y, scoring='f1')
-    #         print("Accuracy: %0.2f (+/- %0.2f) with params: %s and %s" % (scores.mean(), scores.std() * 2, strength, kern))
-    
+    #         print("Accuracy: %0.2f (+/- %0.2f) with params: %s and %s" % (scores.mean(), scores.std(), strength, kern))
 
-    # chips = 'data/chips.csv'
-    # geyser = 'data/geyser.csv'
-    #
-    # X, y = read_from_file(geyser)
 
-    clf = SVC(kernel='rbf', C=100.0)
+    clf = SVC(kernel='linear', C=1.0)
     clf.fit(X, y)
 
     plt.scatter(X[:, 0], X[:, 1], c=colors(y), cmap=plt.cm.Paired)
 
-
     ax = plt.gca()
     xlim = ax.get_xlim()
     ylim = ax.get_ylim()
-
 
     xx = np.linspace(xlim[0], xlim[1], 30)
     yy = np.linspace(ylim[0], ylim[1], 30)
